ca45d66cb820f5ad0351d70afa80ea4384242063
Eran Dagan <eran.dagan7@gmail.com>
2018-07-09 12:48:26 +0300

    Implement deployment preparation command

--- src/Shpasser/GaeSupportL5/Http/routes.php
+++ src/Shpasser/GaeSupportL5/Http/routes.php
@@ -2,11 +2,9 @@
 
 use \Shpasser\GaeSupportL5\Http\Controllers\ArtisanConsoleController;
 
-/**
- * Maintenance routes.
- */
-Route::get('artisan',  array('as' => 'artisan',
-    'uses' => ArtisanConsoleController::class.'@show'));
+
+Route::get('artisan', array('as' => 'artisan',
+    'uses' => ArtisanConsoleController::class . '@show'));
 
 Route::post('artisan', array('as' => 'artisan',
-    'uses' => ArtisanConsoleController::class.'@execute'));
+    'uses' => ArtisanConsoleController::class . '@execute'));
